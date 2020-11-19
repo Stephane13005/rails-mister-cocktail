3d4b08bb4b4499aa98d6e1d56c10d47ed750cf9a
Stéphane Severio <severio.stephane@gmail.com>
2020-11-19 18:16:35 +0100

    front ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+	root "cocktails#index"
 resources :cocktails, only: [ :index, :show, :new, :create ] do
 	resources :doses, only: [ :new, :create]
 end
